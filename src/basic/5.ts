3d07d04438cd749c1ed4f4f57b2325e3cc86fd38
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-05-02 22:00:46 +0300

    base_done

---
+++ src/basic/5.ts
@@ -0,0 +1,29 @@
+// enum DayOfWeek {
+//     Monday,
+//      Tuesday,
+//      Wednesday,
+//      Thursday,
+//      Friday,
+//      Saturday,
+//      Sunday
+//     }
+    
+    
+//     const isWeekend = (day) => {
+//     }
+
+
+enum DayOfWeek {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  }
+  
+  const isWeekend = (day: DayOfWeek): boolean => {
+    const weekendDays = [DayOfWeek.Saturday, DayOfWeek.Sunday];
+    return weekendDays.includes(day);
+  };
